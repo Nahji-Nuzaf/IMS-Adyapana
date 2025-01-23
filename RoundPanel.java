0eb5dcf3b71eb5736b0119b4616bee18642cf526
Nahji Nuzaf <nahji1101@gmail.com>
2025-01-23 12:57:08 +0530

    RoundPanel

---
+++ RoundPanel.java
@@ -0,0 +1,21 @@
+
+package Components;
+
+
+import com.formdev.flatlaf.FlatClientProperties;
+import javax.swing.JPanel;
+
+
+public class RoundPanel extends JPanel{
+    
+    public RoundPanel(){
+        init();
+    }
+
+    private void init() {
+        this.putClientProperty(FlatClientProperties.STYLE, "arc:30");
+        
+    }
+    
+    
+}
